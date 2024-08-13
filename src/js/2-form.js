2783c6c1a482a8b275798c7b7c17ad29e13af9eb
Maryna H <maryna.hamici@yahoo.com>
2024-08-13 14:25:29 +0200

    second update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,27 +1,31 @@
 const form = document.querySelector('.feedback-form');
-const formData = { email: "", message: "" };
+let formData = { email: "", message: "" };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+  }
+});
 
 form.addEventListener('input', event => {
   formData[event.target.name] = event.target.value.trim();
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const savedData = localStorage.getItem('feedback-form-state');
-  if (savedData) {
-    const savedFormData = JSON.parse(savedData);
-    form.elements.email.value = savedFormData.email || '';
-    form.elements.message.value = savedFormData.message || '';
-  }
-});
-
 form.addEventListener('submit', event => {
   event.preventDefault();
+  
   if (!formData.email || !formData.message) {
-    alert('Fill please all fields');
+    alert('Please fill in all fields');
     return;
   }
+  
   console.log(formData);
+  
   localStorage.removeItem('feedback-form-state');
+  formData = { email: "", message: "" };
   form.reset();
 });
